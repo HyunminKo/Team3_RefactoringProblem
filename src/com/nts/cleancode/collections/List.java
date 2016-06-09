4830e449294ca1391dc133c590c8d30e6aae536d
Kohyunmin <hyunmin4578@gmail.com>
2016-06-09 14:46:55 +0900

    Refactoring add method in ListClass

--- src/com/nts/cleancode/collections/List.java
+++ src/com/nts/cleancode/collections/List.java
@@ -2,16 +2,26 @@ package com.nts.cleancode.collections;
 
 public class List extends AbstractCollection {
 	public void add(Object element) {
-		if (!readOnly) {
-			int newSize = size + 1;
-			if (newSize > elements.length) {
-				Object[] newElements =
-					new Object[elements.length + 10];
-				for (int i = 0; i < size; i++)
-					newElements[i] = elements[i];
-				elements = newElements;
+		if (readOnly)
+			return;
+			if (shouldGrow()) {
+				grow();
 			}
-			elements[size++] = element;
-		}
+			addElement(element);
+	}
+	private void addElement(Object element) {
+		elements[size++] = element;
+	}
+
+	private boolean shouldGrow() {
+		return (size + 1) > elements.length;
+	}
+
+	private void grow() {
+		Object[] newElements =
+			new Object[elements.length + 10];
+		for (int i = 0; i < size; i++)
+			newElements[i] = elements[i];
+		elements = newElements;
 	}
 }
